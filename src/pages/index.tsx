8d7d5d6d092dd1ef79a0fb26be86d56e7e2ea9c1
Adam Filchoir <filchoiradam@gmail.com>
2025-01-14 20:41:55 +0700

    Chore: create action cards to interact on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,84 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Card, CardActionArea, CardContent, CardMedia, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 
-export default function test() {
-    return <Box>Hello World</Box>
+export default function LandingPage() {
+    return (
+        <Container>
+            <Box>
+                <Typography>Examtopics</Typography>
+                <Typography>Webscraper</Typography>
+            </Box>
+
+            <Box sx={{
+                display: 'grid',
+                gridTemplateColumns: '1fr 1fr 1fr',
+                gap: 10
+            }}>
+                <Card elevation={3} sx={{
+                    justifyItems: 'center',
+                }}>
+                    <CardActionArea>
+                        <CardMedia
+                            component='img'
+                            height={'300px'}
+                            image="landing\scraped_html.png"
+                            alt="A random Python Script?"
+                            sx={{
+                                objectPosition: 'left top'
+                            }}
+                        />
+                        <CardContent>
+
+                            <Typography fontSize={'25px'} gutterBottom fontWeight={600}>
+                                Scrape the Internet!
+
+                            </Typography>
+                            <Typography fontSize={'17px'} sx={{
+                            }}>
+                                Scrape Exam Topics to find sample questions for your Examination
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+                <Card elevation={3} sx={{
+                    justifyItems: 'center',
+
+                }}>
+                    <CardActionArea>
+                        <CardMedia
+                            component='img'
+                            height={'300px'}
+                            image=""
+                            alt="Exam Folders?"
+                        />
+                        <CardContent>
+                    <Typography>
+                        View Exam Repository
+
+                    </Typography>
+                    </CardContent>
+                    </CardActionArea>
+                </Card>
+                <Card elevation={3} sx={{
+                    justifyItems: 'center',
+
+                }}>
+                    <CardActionArea>
+                        <CardMedia
+                            component='img'
+                            height={'300px'}
+                            image=""
+                            alt="Simulate?"
+                        />
+                        <CardContent>
+                    <Typography>
+
+                        Simulate Exam
+                    </Typography>
+                    </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Box>
+        </Container>
+    )
 }
