f3a82f0344a77ee6d3a1bc66f1f2665abb19066f
Adam Filchoir <filchoiradam@gmail.com>
2025-01-17 22:02:53 +0700

    Fix: footer text change

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 
 export default function Footer() {
@@ -8,13 +8,25 @@ export default function Footer() {
     return (
         <Box
         sx={{
-            width: '100%',
             backgroundColor: '#1976d2',
-            height: 50
+            height: 50,
+            paddingX: 3,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems:'center'
         }}>
             <Typography>
                 Our Contact Information
             </Typography>
+            <Stack direction={'row'} gap={3}>
+
+            <Typography>
+                About Project Page?
+            </Typography>
+            <Typography>
+                GitHub link
+            </Typography>
+            </Stack>
         </Box>
     )
 }
