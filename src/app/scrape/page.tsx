41775a096f8b71ab100179b7feab0cff3ee75732
Adam Filchoir <filchoiradam@gmail.com>
2025-01-18 09:32:19 +0700

    Chore: imprvoe UI further

--- src/app/scrape/page.tsx
+++ src/app/scrape/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Autocomplete, Box, Chip, Container, Paper, Stack, TextField, Typography } from "@mui/material"
+import { Autocomplete, Box, Button, ButtonBase, Chip, Container, Paper, Stack, TextField, Typography } from "@mui/material"
 import PROVIDER_LIST from 'dumps/PROVIDER_LIST.json'
 import { useState } from "react"
 
@@ -24,56 +24,97 @@ export default function ScrapePage() {
 
 
     return (
-        <Box maxWidth={'1600px'}justifySelf={'center'} marginY={5}>
-            <Typography>Type an Exam or </Typography>
+        <Box maxWidth={'1600px'} justifySelf={'center'} marginY={5}>
 
+            <Box justifySelf={'center'} display={'flex'} marginBottom={3}>
+                <Chip sx={{
+                    alignSelf: 'center',
+                    minWidth: '100px',
+                    fontSize: '18px'
+                }} label={selectedExam.provider} />
+                <Box sx={{
+                    minWidth: '500px',
+                    marginX: 4,
+                    justifyItems: 'center',
+                    alignContent: 'center'
+                }}>
+                    <Typography fontSize={'20px'}>
+                        {selectedExam.exam}
+                    </Typography>
+
+                </Box>
+                <Button size="large" disabled={selectedExam.exam == ''}>
+                    Scrape!
+                </Button>
+            </Box>
 
             <Stack gap={3}>
                 <Box>
                     <Autocomplete
-                    fullWidth
-                    sx={{
-                        width: '1200px',
-                        justifySelf: 'center'
-                    }}
-                    options={providers}
-                    renderInput={(params) => <TextField {...params} label="Search for an Exam / Exam Code / Provider " />}
+                        fullWidth
+                        sx={{
+                            width: '1200px',
+                            justifySelf: 'center'
+                        }}
+                        options={providers}
+                        renderInput={(params) => <TextField {...params} label="Search for an Exam / Exam Code / Provider " />}
                     />
                 </Box>
 
 
                 <Box display={'grid'} gridTemplateColumns={'1.2fr 2fr'} gap={3}>
                     <Paper>
-                    <Box display={'flex'} flexWrap={"wrap"} gap={1} justifyContent={'space-around'} margin={2}>
-                        {PROVIDER_LIST.map((pro, i) => (
-                            <Chip key={i} label={pro.provider} color={selectedExam.provider == pro.provider ? 'primary' : 'default'} onClick={() => {
-                                if (selectedExam.provider != pro.provider) {
-                                    setSelectedExam({...selectedExam, provider: pro.provider, index: pro.index})
-                                } else {
-                                    setSelectedExam({ ...selectedExam, provider: '', index: null})
-                                }
-                            }}
-                            
-                            />
-                        ))}
+                        <Box display={'flex'} flexWrap={"wrap"} gap={1} justifyContent={'space-around'} margin={2}>
+                            {PROVIDER_LIST.map((pro, i) => (
+                                <Chip key={i} label={pro.provider} color={selectedExam.provider == pro.provider ? 'primary' : 'default'} onClick={() => {
+                                    if (selectedExam.provider != pro.provider) {
+                                        setSelectedExam({ exam: '', provider: pro.provider, index: pro.index })
+                                    } else {
+                                        setSelectedExam({ exam: '', provider: '', index: null })
+                                    }
+                                }}
 
-                    </Box>
+                                />
+                            ))}
+
+                        </Box>
 
 
                     </Paper>
                     <Paper>
                         {selectedExam.index && (
-                        <Box>
-                            {PROVIDER_LIST[selectedExam.index - 1].exams.map((exam,i) => (
-                            <Box key={i} display={'flex'} alignItems={'center'}>
-                                    <Typography textOverflow={'ellipsis'}>{exam.exam_name}</Typography>
-                                    {exam.popular && (
-                                        <Chip label="Popular" color="info" />
-                                    )}
-                                </Box>
-                            ))}
+                            <Box>
+                                {PROVIDER_LIST[selectedExam.index - 1].exams.map((exam, i) => (
+                                    <ButtonBase key={i} sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'start',
+                                        height: 45,
+                                        width: '100%',
+                                        gap: 2,
+                                        paddingX: 2,
+                                        '&:hover': {
+                                            backgroundColor: '#1976d2'
+                                        }
+                                    }} onClick={() => {
+                                        if (selectedExam.exam != exam.exam_name) {
+                                            setSelectedExam({ ...selectedExam, exam: exam.exam_name })
+                                        } else {
+                                            setSelectedExam({ ...selectedExam, exam: exam.exam_name })
+                                        }
+                                    }}>
 
-                        </Box>
+                                        <Typography textOverflow={'ellipsis'}>{exam.exam_name}</Typography>
+                                        {exam.popular && (
+                                            <>
+                                            <Typography>&bull;</Typography>
+                                            <Chip label="Popular" color="info" />
+                                            </>
+                                        )}
+                                    </ButtonBase>
+                                ))}
+
+                            </Box>
 
                         )}
 
