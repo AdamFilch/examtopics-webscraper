b224e8a83ceca44ba3555d0b3eb5ab6ca496386d
JoozKelly <joozkelly12@gmail.com>
2025-01-25 16:16:34 +0800

    Preview Page is left with CSS

--- src/app/repository/page.tsx
+++ src/app/repository/page.tsx
@@ -1,14 +1,23 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
 import React from "react";
 import AWS_CERTIFIED_CLOUD_QUESTIONS from 'dumps/exam_papers/AWS Certified Cloud Practitioner CLF-C02.json'
 import EXAMPLE_QUESTION_DUMP from 'dumps/exam_papers/EXAMPLE_QUESTION_DUMP.json'
 
+
+
 export default function test() {
 
+    console.log(AWS_CERTIFIED_CLOUD_QUESTIONS)
     const amount = AWS_CERTIFIED_CLOUD_QUESTIONS.length
     const exampl_question_dump = EXAMPLE_QUESTION_DUMP.length
-    return <Box>REPOSITYRORUISOFIHS:
-        {amount}
+    return <Box sx={{
+        display: 'flex',
+        justifyContent: 'center'
+    }}>
 
+    <Box>
+        <Typography> There are {amount} exam questions </Typography>
+        <Typography> There are {exampl_question_dump} exam questions </Typography>
+    </Box>
     </Box>
 }
