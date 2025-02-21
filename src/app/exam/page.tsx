7cb92aac1489072929562ed010d7be446d580f89
JoozKelly <joozkelly12@gmail.com>
2025-02-21 19:55:07 +0800

    Exam Page, Exam Title

--- src/app/exam/page.tsx
+++ src/app/exam/page.tsx
@@ -17,15 +17,15 @@ export default function test() {
 
 
     //to set the question number 
-    const [questionNumber, setQuestionNumber] = useState(0) 
+    const [questionNumber, setQuestionNumber] = useState(0)
 
 
     //to extract exam provider list from JSON file
-    const autoCompleteList = PROVIDER_LIST.map((list) => list.provider)  
+    const autoCompleteList = PROVIDER_LIST.map((list) => list.provider)
 
 
     //to extract the nested array of exam names from the list of providers
-    const exams = PROVIDER_LIST.map((list) => list.exams).flat() 
+    const exams = PROVIDER_LIST.map((list) => list.exams).flat()
 
     // const ExList = exams.map((pro) => pro.exam_name)
 
@@ -45,69 +45,69 @@ export default function test() {
     //actual HTML components
     return <Box>
 
-    {/* autocomplete */}
+        {/* autocomplete */}
 
         <Box justifySelf={'center'}>
-        <Typography sx={{
-            margin:2
-        }}>Choose the exam you would like to take</Typography>
-        <Autocomplete
-            options={autoCompleteList}
-            sx={{ 
-                width: 300,
-                margin:2
-            }}
-            renderInput={(params) => <TextField {...params} label="Search for an Exam / Exam Code / Provider " />}
-        />
+            <Typography sx={{
+                margin: 2
+            }}>Choose the exam you would like to take</Typography>
+            <Autocomplete
+                options={autoCompleteList}
+                sx={{
+                    width: 300,
+                    margin: 2
+                }}
+                renderInput={(params) => <TextField {...params} label="Search for an Exam / Exam Code / Provider " />}
+            />
         </Box>
-        
-    {/* Exam Name Selection */}
+
+        {/* Exam Name Selection */}
 
         <Box justifySelf={'center'}>
-            
+            <Typography> {AWS_CERTIFIED_CLOUD_QUESTIONS[questionNumber].title} </Typography>
         </Box>
 
-    {/* Next and back button */}
+        {/* Next and back button */}
 
         <Box justifySelf={'center'} display={'flex'} justifyContent={'space-between'} width={'100%'}>
             <Button onClick={handleBack} sx={{
-                margin:2,
-                marginLeft:5,
-                fontSize:16,
+                margin: 2,
+                marginLeft: 5,
+                fontSize: 16,
             }}>Back</Button>
 
             <Button onClick={handleNext} sx={{
-                margin:2,
-                marginLeft:5,
-                fontSize:16,
+                margin: 2,
+                marginLeft: 5,
+                fontSize: 16,
             }}>Next</Button>
         </Box>
-        
-    {/* This is for questions */}
+
+        {/* This is for questions */}
 
         <Box justifySelf={'center'} display={'flex'} marginBottom={2} marginTop={1} marginLeft={10} marginRight={10}>
             <Box>
                 <Typography sx={{
-                    fontSize:18
+                    fontSize: 18
                 }}>{AWS_CERTIFIED_CLOUD_QUESTIONS[questionNumber].question}</Typography>
             </Box>
 
 
         </Box>
 
-    {/* This is for answers */}
+        {/* This is for answers */}
 
         <Paper>
-        <Box paddingBottom={5} paddingTop={5} fontSize={20}> 
-            <RadioGroup sx={{
-                marginLeft:15,
-            }}>
-                {AWS_CERTIFIED_CLOUD_QUESTIONS[questionNumber].choices.map((ChoiceNumber) => (
-                    <FormControlLabel key={ChoiceNumber} value={ChoiceNumber} control={<Radio />} label={ChoiceNumber} />
+            <Box paddingBottom={5} paddingTop={5} fontSize={20}>
+                <RadioGroup sx={{
+                    marginLeft: 15,
+                }}>
+                    {AWS_CERTIFIED_CLOUD_QUESTIONS[questionNumber].choices.map((ChoiceNumber) => (
+                        <FormControlLabel key={ChoiceNumber} value={ChoiceNumber} control={<Radio />} label={ChoiceNumber} />
 
-                ))}
-            </RadioGroup>
-        </Box>
+                    ))}
+                </RadioGroup>
+            </Box>
         </Paper>
     </Box>
 
