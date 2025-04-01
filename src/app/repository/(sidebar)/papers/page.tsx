ba1bafd68ff36bb282e0ef32fc7637bf7b9be3d6
Adam Filchoir <filchoiradam@gmail.com>
2025-04-01 21:50:50 +0700

    Chore: create sample test paper buttons

--- src/app/repository/(sidebar)/papers/page.tsx
+++ src/app/repository/(sidebar)/papers/page.tsx
@@ -1,22 +1,48 @@
-import { Box, Card, CardActionArea, Typography } from "@mui/material";
+import { Box, Card, CardActionArea, CardContent, Typography } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 
 export default function PapersPage() {
     return (
         <Box>
-            <Box>
-                <CardActionArea>
-                    <Card>
-                        <AddIcon />
-                    </Card>
-                </CardActionArea>
-                <CardActionArea>
-                    <Card>
+            <Box sx={{
+                display: 'flex',
+                gap: 3,
+                padding: 3
+            }}>
+                <Card >
+                    <CardActionArea>
+                        <CardContent sx={{
+                            width: 150,
+                            height: 225,
+                            alignContent: 'center',
+                            alignItems: 'center',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            gap: 3
+                        }}>
+                            <AddIcon sx={{ fontSize: '35px'}} />
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+                <Card>
+                    <CardActionArea>
+                        <CardContent sx={{
+                            width: 150,
+                            height: 225,
+                            alignContent: 'center',
+                            alignItems: 'center',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            flexDirection: 'column',
+                            gap: 3
+                        }}>
+
                         <Typography>Test name</Typography>
                         <Typography>X Questions</Typography>
                         <Typography>60 Min</Typography>
-                    </Card>
-                </CardActionArea>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
             </Box>
         </Box>
     )
