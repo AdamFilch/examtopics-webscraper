ebcb044996bc648870c226905f5d4a9c9a82c995
Adam Filchoir <filchoiradam@gmail.com>
2025-04-04 23:09:40 +0800

    Chore: add routes to repository sidebar

--- src/components/repository/sidebar.tsx
+++ src/components/repository/sidebar.tsx
@@ -1,14 +1,21 @@
+'use client'
 import { Box, Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 
 export function RepoSidebar() {
+    const router = useRouter();
     return (
 
         <Box display={'flex'} gap={2} padding={2} flexDirection={"column"}>
-            <Button variant="contained">
+            <Button variant="contained" onClick={() => {
+                router.replace('/repository')
+            }}>
                 Questions
             </Button>
-            <Button variant="contained">
+            <Button variant="contained" onClick={() => {
+                router.replace('/repository/papers')
+            }}>
                 Papers
             </Button>
         </Box>
